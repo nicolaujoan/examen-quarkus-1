f74a826dcd2103b8a36943794e3c2fb30a869f67
Joan Nicolau <joannicolau23@gmail.com>
2022-06-02 21:57:44 +0200

    feat(ResourcesOlli): :white_check_mark: tests in green!
    Implemented all resources

--- src/main/java/edu/poniperro/ResourcesOlli.java
+++ src/main/java/edu/poniperro/ResourcesOlli.java
@@ -1,12 +1,22 @@
 package edu.poniperro;
 
 import javax.inject.Inject;
+import javax.print.attribute.standard.Media;
+import javax.transaction.Transactional;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
 
-@Path("/hello")
+import edu.poniperro.dominio.Item;
+import edu.poniperro.dominio.Orden;
+import edu.poniperro.dominio.Usuaria;
+
+@Path("/")
 public class ResourcesOlli {
 
     @Inject
@@ -14,7 +24,52 @@ public class ResourcesOlli {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
+    @Path("/welcome")
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return "Wellcome Ollivanders!";
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/usuaria/{nombre}")
+    public Response getUsuariaByName(@PathParam("nombre") String name) {
+        Usuaria usuaria = service.cargaUsuaria(name);
+        
+        return !usuaria.getNombre().isEmpty()
+            ? Response.ok(usuaria).build()
+            : Response.status(404).build();
+    }
+
+    @POST
+    @Transactional
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/ordena")
+    public Response getPedido(Orden orden) {
+        Usuaria user = orden.getUser();
+        Item item = orden.getItem();
+        Orden comanda = service.comanda(user.getNombre(), item.getNombre());
+
+        return comanda != null 
+            ? Response.status(201).entity(comanda).build()
+            : Response.noContent().status(404).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/pedidos/{usuaria}")
+    public Response getPedidos(Usuaria usuaria) {
+        List<Orden> ordenes = service.cargaOrden(usuaria.getNombre());
+        return Response.ok(ordenes).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/item/{nombre}")
+    public Response getItem(@PathParam("nombre") String nombre) {
+        Item item = service.cargaItem(nombre);
+
+        return !item.getNombre().isEmpty()
+            ? Response.ok(item).build()
+            : Response.noContent().status(404).build();
     }
 }
